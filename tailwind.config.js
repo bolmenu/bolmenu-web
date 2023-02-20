42739dfd3620f9bde42364250c364b4e6fb90f95
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-02-20 22:39:18 +0300

    [feat] Tailwindcss init.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
+  theme: {
+    fontFamily: {
+      sans: "Poppins",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
