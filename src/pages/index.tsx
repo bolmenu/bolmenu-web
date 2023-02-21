6c35f119734dc7c6fd0d5aadaef587dbed76d141
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-02-21 20:16:30 +0300

    [feat] Hero component development.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <Hero />
+      <div className="container mx-auto">
         <Button onClick={() => console.log("click!")}>Test Button</Button>
-      </main>
+      </div>
     </>
   );
 }
