4b0c6bdaa7162de69e492e899b2617f833c3102f
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-02-22 21:49:58 +0300

    [feat] Component development.

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,20 @@
+export enum BUTTON_TYPES {
+  PRIMARY = "PRIMARY",
+  SECONDARY = "SECONDARY",
+}
+
 interface ButtonProps {
   children: React.ReactNode;
+  type?: BUTTON_TYPES.PRIMARY | BUTTON_TYPES.SECONDARY;
   onClick?: () => void;
 }
 
 const Button = ({ children, onClick }: ButtonProps) => {
   return (
-    <button className="px-[40px] py-[15px] bg-[#FFCB45] hover:bg-[#FCC537] transition rounded-md text-base font-medium text-[#3A6451]" onClick={onClick}>
+    <button
+      className="px-[40px] py-[15px] bg-[#FFCB45] hover:bg-[#FCC537] transition rounded-md text-base font-medium text-[#3A6451] w-full"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
