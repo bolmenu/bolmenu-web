03dccbb4df85cb8a8d390aec3ebee95058c67b5a
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-02-28 20:10:37 +0300

    [feat] Feature box development.

--- src/components/feature-box/index.tsx
+++ src/components/feature-box/index.tsx
@@ -10,13 +10,13 @@ interface IFeatureBox {
 }
 
 export default ({ icon, title, detail }: IFeatureBox) => (
-  <div className="w-1/3">
+  <div className="w-1/3 flex gap-[30px] px-[15px] py-[60px]">
     <figure>
-      <FontAwesomeIcon icon={icon} />
+      <FontAwesomeIcon icon={icon} size="3x" color="#054f48" />
     </figure>
     <article>
-      <Typography theme={TypographyTypes.H4}>{title}</Typography>
-      <Typography theme={TypographyTypes.P}>{detail}</Typography>
+      <Typography theme={TypographyTypes.H4} className="text-[#054f48]">{title}</Typography>
+      <Typography theme={TypographyTypes.P} className="text-[#8d8d8d]">{detail}</Typography>
     </article>
   </div>
 );
