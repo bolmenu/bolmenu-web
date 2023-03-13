0371675ffacfed0772abaf96259a6d52b59548d1
Yasin KALKAN <yasinburakkalkan@gmail.com>
2023-03-13 20:59:37 +0300

    [feat] Responsive development.

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -3,12 +3,12 @@ import { TypographyTypes } from "@/styles/theme/type";
 
 export default () => {
   return (
-    <div className="relative pt-[50px]">
-      <div className="absolute top-0 left-0 z-[-1] bg-[url('/assets/img/hero-background.jpg')] bg-cover bg-center w-[100vw] h-[70vh] min-h-[930px]"></div>
+    <div className="relative pt-[50px] pb-[50px] lg:pb-[0px]">
+      <div className="absolute top-0 left-0 z-[-1] bg-[url('/assets/img/hero-background.jpg')] bg-cover bg-center w-[100vw] h-[100%] lg:h-[70vh] min-h-[930px]"></div>
       <div className="container mx-auto relative">
         <Header />
-        <section className="flex mt-[200px] gap-[30px] min-h-[626px] max-h-[calc(70vh_-_(200px_+_54px_+_50px))]">
-          <article className="w-[50%]">
+        <section className="flex flex-col lg:flex-row md:mt-[100px] lg:mt-[200px] gap-[30px] md:min-h-[auto] lg:min-h-[626px] md:max-h-[none] lg:max-h-[calc(70vh_-_(200px_+_54px_+_50px))]">
+          <article className="w-[100%] lg:w-[50%] text-center lg:text-left">
             <Typography theme={TypographyTypes.H1} className="text-[#ffcb45]">
               Menü oluşturmanın en kolay ve ucuz yolu
             </Typography>
@@ -21,7 +21,7 @@ export default () => {
               %40 daha fazla bahşiş almanızı sağlar.
             </Typography>
           </article>
-          <figure className="w-[50%] relative">
+          <figure className="w-[100%] lg:w-[50%] relative">
             <div className="w-[100%] lg:h-[497px] xl:h-[625px] 2xl:h-[753px] bg-[#ffcb45] rounded-full flex items-center justify-center pb-[100px]">
               <img
                 src="/assets/img/demo.webp"
